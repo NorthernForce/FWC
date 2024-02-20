df002d7dd5cd5d840111f7bc83063dedb2e7be35
FRCTeam172 <team172@northernforcerobotics.org>
2024-02-20 11:15:12 -0500

    Change ip address to robot address

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { NT4Provider } from '@frc-web-components/react';
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
-    <NT4Provider address="localhost">
+    <NT4Provider address="10.1.72.2">
       <App />
     </NT4Provider>
   </React.StrictMode>
