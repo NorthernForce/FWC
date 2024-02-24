8d0c1e1e39ac87685a1c979cbc2096573158c754
cml1010101 <cml1010101@gmail.com>
2024-02-24 13:58:04 -0500

    Crab rave button

--- src/components/ServiceDashboard.tsx
+++ src/components/ServiceDashboard.tsx
@@ -6,16 +6,21 @@ const ServiceDashboard = () => {
     return (
         <>
             <div className="service-container">
-                <RobotCommand name="Calibrate Swerve Drive" className="service-command" source-key="/FWC/calibrate_drive"/>
+                <div className="command-bar">
+                    <RobotCommand name="Calibrate Swerve Drive" className="service-command" source-key="/FWC/calibrate_drive"/>
+                    <RobotCommand name="Play Crab Rave" className="service-command" source-key="/FWC/play_crab_rave"/>
+                </div>
                 <Field className="service-field" rotationUnit="rad">
                     <FieldRobot pose={pose}/>
                 </Field>
                 <div className="service-cameras">
                     <Canvas className="service-camera">
-                        <CanvasMjpgStream className="service-camera-stream" origin={[0,0]} crosshairColor="white" width={320} height={240} srcs={["mjpg:http://10.1.72.47:1181/?action=stream"]}/>
+                        <CanvasMjpgStream className="service-camera-stream" origin={[0,0]} crosshairColor="white" width={320} height={240}
+                            srcs={["mjpg:http://10.1.72.47:1181/?action=stream"]}/>
                     </Canvas>
                     <Canvas className="service-camera">
-                        <CanvasMjpgStream className="service-camera-stream" origin={[0,0]} crosshairColor="white" width={320} height={240} srcs={["mjpg:http://10.1.72.31:1181/?action=stream"]}/>
+                        <CanvasMjpgStream className="service-camera-stream" origin={[0,0]} crosshairColor="white" width={320} height={240}
+                            srcs={["mjpg:http://10.1.72.31:1181/?action=stream"]}/>
                     </Canvas>
                 </div>
             </div>
